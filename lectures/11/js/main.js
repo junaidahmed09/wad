b32b4d9634b19e3ece01fc5cf2236d41cdd23f62
junaidahmed09 <junaidkol09@gmail.com>
2018-11-23 16:35:26 +0500

    Class Activity (Javascript)

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -2,10 +2,13 @@
 //-----------  STEP - 1 ----------------------
 //============================================
 
+//In this step we have created an array (default size 0), and in the add fucntion we have created a var task
+//which basically gets the userinput, and then it is inserted into the array todos via push fucntion call.
+//the last line basically display's the contents of array one by one separated by a comma.
 
 /*var todos = [];
 function add() {
-    var task = document.getElementById("task").value;
+    var task = document.getElementById('task').value;
     todos.push(task);
     document.getElementById('todos').innerText = todos;
 }*/
@@ -15,8 +18,13 @@ function add() {
 //-----------  STEP - 2 ----------------------
 //============================================
 
+//In this step the initial steps are same, the changes made are that a new function show() has been created,
+//in this function we create a list element the in the for loop we add items of the list from the array todos
+//the one problem which we faced in this step is that whenever the user add's a new entry to the list, then the previous
+//entries also gets displayed with it.
 
-/*var todos = [];
+/*
+var todos = [];
 function add() {
     var task = document.getElementById('task').value;
     todos.push(task);
@@ -34,19 +42,25 @@ function show() {
         ul.appendChild(li);
     }
     document.getElementById('todos').appendChild(ul);
-}*/
+}
+*/
 
 
 //============================================
 //-----------  STEP - 3 ----------------------
 //============================================
 
+//In this step we have added a new function getTodos(), this function basically gets data from the local storage of the browser,
+// then ocnvert the data to string. In the add function the changes which are made is the if and else condition in which a error is generated
+//whenever the user enters a whitespace, one more change is that to store the data to local storage we have to convert it to string.
+//In the show function there is a change in the for loop where we have added a delete button to delete a entry from the list.
+//the last call of function show() after the function body is to assure that the display doesn't hide when we refresh the page.
 
-/*
-function getTodos() {
+
+/*function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
-    if(todos_str !== null)
+    if(todos_str != null)
         todos = JSON.parse(todos_str);
     return todos;
 }
@@ -60,8 +74,8 @@ function add() {
         document.getElementById('message').style.display = 'none';
     }
     var todos = getTodos();
-    todos.push({task: task});
-    document.getElementById('task').value = '';
+    todos.push({task:task});
+    document.getElementById('task').value;
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
@@ -83,17 +97,17 @@ function show() {
     }
     document.getElementById('todos').appendChild(ul);
 }
-show();
-*/
-
+show();*/
 
 
 //============================================
 //-----------  STEP - 4 ----------------------
 //============================================
 
+//In this step we have added the fucntion remove, which removes the enteries in the todos list if the delete button is pressed.
 
-/*function getTodos() {
+/*
+function getTodos() {
     var todos = [];
     var todos_str = localStorage.getItem('todo');
     if(todos_str !== null)
@@ -162,15 +176,92 @@ function isDone(e) {
     localStorage.setItem('todo',JSON.stringify(todos));
     show();
 }
-show();*/
-
-
-
+show();
+*/
 
 
 //============================================
 //-----------  STEP - 5 ----------------------
 //============================================
+
+function strikeOut() {
+    document.getElementsByClassName("todos").style.textDecoration="line-through";
+}
+
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger " id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'
+            + '<button class="btn btn-primary" id="' + i + '">' +
+               '<i class="fas fa-pen-square"></i>' +
+               '<span class="d-none d-sm-inline"> Edit </span> </button>' ;
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
 
 /*
 
@@ -180,8 +271,6 @@ CLASS ACTIVITY (HOME WORK) :
 HINT: use text-decoration:line-through; property of CSS
 
 2- Enable Editing todos in text field to update text
-
-
 
 */
 
